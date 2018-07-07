1c23917aeaaaabb0dd21ec603c474c8806e8d98b
CodeingBoy <CodeingBoy@gmail.com>
2018-07-07 23:56:38 +0800

    Add secret.js, please fill your tencent SDK key here

---
+++ secret.js
@@ -0,0 +1,9 @@
+const tencentMapSdkKey = 'YOUR_TENCENT_MAP_SDK_HERE';
+
+const exportObject = {
+  getTencentMapSdkKey: function() {
+    return tencentMapSdkKey;
+  }
+};
+
+module.exports = exportObject;
